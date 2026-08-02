c52f2443899cffda0c185e34667662ee1dfe231e
karim09361180212-design <karim09361180212@gmail.com>
2026-08-03 02:37:04 +0330

    Create server.js

---
+++ backend/backend/backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to Souqna API"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
